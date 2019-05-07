f8542e43c28bdda9a1ae7dcec538a080a5a7a719
theredbutton731 <reachadi1512@gmail.com>
2019-05-06 20:34:49 -0400

    Improved cnn

--- cnn.py
+++ cnn.py
@@ -4,7 +4,7 @@ from keras.preprocessing.image import load_img
 from keras.preprocessing.image import save_img
 from keras.preprocessing.image import img_to_array
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 from keras.models import load_model
 import numpy as np
@@ -12,23 +12,18 @@ from sklearn.model_selection import train_test_split
 import os
 import matplotlib.pyplot as plt
 
-original_parasitized = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\Parasitized\\'
-original_uninfected = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\Uninfected\\'
-resized_parasitized = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\Parasitized\\'
-resized_uninfected = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\Uninfected\\'
-original_images = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\'
-resized_images = 'C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\'
+original_parasitized = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\Parasitized\\"
+original_uninfected = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\Uninfected\\"
+resized_parasitized = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\Parasitized\\"
+resized_uninfected = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\Uninfected\\"
+original_images = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\original_cell_images\\"
+resized_images = "C:\\Users\\reach\\Documents\\Stuff\\Machine Learning\\Malaria-Detector\\resized_cell_images\\"
 
-batch_size, num_classes, epochs = 128, 2, 10
+batch_size, num_classes, epochs = 128, 2, 15
 channels, img_rows, img_cols = 3, 32, 32
 num_parasitized_images, num_uninfected_images = 13779, 13779
 
-class LossHistory(keras.callbacks.Callback):
-    def on_train_begin(self, logs = {}):
-        self.losses = []
-
-    def on_batch_end(self, batch, logs = {}):
-        self.losses.append(logs.get('loss'))
+global data, labels, data_train, data_test, labels_train, labels_test, input_shape
 
 def resize_images():
 	image_count = 0
@@ -50,83 +45,103 @@ def resize_images():
 			img_array = img_to_array(img)
 			save_img(resized_uninfected + filename, img_array)
 			image_count += 1
-			if image_count % 1000 == 0:
-				print("Resizing uninfected image " + str(image_count))
+			if (image_count - num_parasitized_images) % 1000 == 0:
+				print("Resizing uninfected image " + str(image_count - num_parasitized_images))
 
 	print("Finished resizing uninfected cell images")
 
-def create_datasets():
-	data = np.ndarray(shape=(num_parasitized_images + num_uninfected_images, img_rows, img_cols, channels), dtype=np.float32)
-	image_count = 0
-	for filename in os.listdir(resized_parasitized):
-		img = load_img(resized_parasitized + filename)
-		img_array = img_to_array(img) # shape is (64, 64, 3)
-		img_array.reshape((img_rows, img_cols, channels))
-		data[image_count] = img_array
-		image_count += 1
-		if image_count % 1000 == 0:
+def get_data():
+	global data, labels
+
+	if input("Load saved shuffled image data and labels?\n") in ["Yes", "yes"]:
+		data, labels = np.load("data.npy"), np.load("labels.npy")
+		print("Shuffled image data and labels loaded from data.npy and labels.npy")
+
+	else:
+		if input("Resize images?\n") in ["Yes", "yes"]:
+			resize_images()
+
+		data = np.ndarray(shape=(num_parasitized_images + num_uninfected_images, img_rows, img_cols, channels), dtype=np.float32)
+		image_count = 0
+		for filename in os.listdir(resized_parasitized):
+			img = load_img(resized_parasitized + filename)
+			img_array = img_to_array(img) # shape is (64, 64, 3)
+			img_array.reshape((img_rows, img_cols, channels))
+			data[image_count] = img_array
+			image_count += 1
+			if image_count % 1000 == 0:
 				print("Gathering parasitized cell image " + str(image_count))
 
-	print("Finished gathering parasitized cell images")
+		print("Finished gathering parasitized cell images data")
 
-	for filename in os.listdir(resized_uninfected):
-		img = load_img(resized_uninfected + filename)
-		img_array = img_to_array(img) # shape is (64, 64, 3)
-		img_array.reshape((img_rows, img_cols, channels))
-		data[image_count] = img_array
-		image_count += 1
-		if image_count % 1000 == 0:
-				print("Gathering uninfected image " + str(image_count))
+		for filename in os.listdir(resized_uninfected):
+			img = load_img(resized_uninfected + filename)
+			img_array = img_to_array(img) # shape is (64, 64, 3)
+			img_array.reshape((img_rows, img_cols, channels))
+			data[image_count] = img_array
+			image_count += 1
+			if (image_count - num_parasitized_images) % 1000 == 0:
+				print("Gathering uninfected image " + str(image_count - num_parasitized_images))
 
-	print("Finished gathering uninfected cell images")
+		print("Finished gathering uninfected cell images data")
+
+		labels = np.ndarray(shape = (num_parasitized_images + num_uninfected_images), dtype = int)
+		for n in range(0, num_parasitized_images):
+			labels[n] = 1
+		for n in range(num_parasitized_images, num_parasitized_images + num_uninfected_images):
+			labels[n] = 0
+		print("Generated labels")
+
+		labels = keras.utils.to_categorical(labels, num_classes)
+		print("One-hot encoded labels")
+
+		idx = np.random.permutation(len(data))
+		data, labels = data[idx], labels[idx]
+		print("Shuffled data and labels")	
+
+		if input("Save shuffled image data and labels?\n") in ["Yes", "yes"]:
+			np.save(file = "data", arr = data)
+			np.save(file = "labels", arr = labels)
+			print("Shuffled image data and labels saved in data.npy and labels.npy")
+
+def create_datasets(test_proportion):
+	global data, labels, data_train, data_test, labels_train, labels_test, input_shape
 
 	input_shape = (img_rows, img_cols, 3)
 
-	labels = np.ndarray(shape = (num_parasitized_images + num_uninfected_images), dtype = int)
-	for n in range(0, num_parasitized_images):
-		labels[n] = 1
-	for n in range(num_parasitized_images, num_parasitized_images + num_uninfected_images):
-		labels[n] = 0
-	
-	print("Generated labels")
-
-	labels = keras.utils.to_categorical(labels, num_classes)
-
-	print("One-hot encoded labels")
-
-	idx = np.random.permutation(len(data))
-	data, labels = data[idx], labels[idx]
-
-	print("Shuffled data and labels")
-
-	data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size = 0.20)
-	
-	print("Split training and test sets")
-
-	data_train /= 255
-	data_test /= 255
-
-	print("Normalized data")
-
-	return data_train, data_test, labels_train, labels_test, input_shape
+	load_sets = input("Load shuffled training and test sets?\n")
+	if load_sets in ["Yes", "yes"]:
+		data_train, data_test, labels_train, labels_test = np.load("data_train.npy"), np.load("data_test.npy"), np.load("labels_train.npy"), np.load("labels_test.npy")
+		print("Loaded shuffled training and test sets")
+	else:
+		data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size = test_proportion)
+		
+		data_train /= 255
+		data_test /= 255
+		print("Split and normalized shuffled data")
+		
+		if input("Save split and normalized shuffled data?\n") in ["Yes", "yes"]:
+			np.save(file = "data_train.npy", arr = data_train)
+			np.save(file = "data_test.npy", arr = data_test)
+			np.save(file = "labels_train.npy", arr = labels_train)
+			np.save(file = "labels_test.npy", arr = labels_test)
+			print("Split and normalized shuffled data saved in data_train.npy, data_test.npy, labels_train.npy, and labels_test.npy")
 
 def train_model(input_shape):
 	model = Sequential()
-	model.add(Conv2D(32, kernel_size = (3, 3), activation = 'relu', input_shape = input_shape))
-	model.add(Conv2D(32, kernel_size = (3, 3), activation = 'relu'))
+	model.add(Conv2D(32, kernel_size = (3, 3), activation = "relu", input_shape = input_shape))
+	model.add(Conv2D(32, kernel_size = (3, 3), activation = "relu"))
 	model.add(MaxPooling2D(pool_size = (2, 2)))
-	model.add(Dropout(0.25))
+	model.add(BatchNormalization())
 	model.add(Flatten())
-	model.add(Dense(128, activation = 'relu'))
-	model.add(Dropout(0.5))
-	model.add(Dense(num_classes, activation = 'softmax'))
+	model.add(Dense(128, activation = "relu"))
+	model.add(BatchNormalization())
+	model.add(Dense(num_classes, activation = "softmax"))
 	
-	model.compile(loss = keras.losses.categorical_crossentropy, optimizer = keras.optimizers.Adadelta(), metrics = ['accuracy'])
+	model.compile(loss = keras.losses.categorical_crossentropy, optimizer = keras.optimizers.Adadelta(), metrics = ["accuracy"])
 
 	print("Created CNN model")
 
-	# history = LossHistory()
-	# model.fit(data_train, labels_train, batch_size = batch_size, epochs = epochs, verbose = 1, callbacks = [history])
 	history = model.fit(data_train, labels_train, batch_size = batch_size, epochs = epochs, verbose = 1)
 	
 	print("Finished training CNN model")
@@ -134,55 +149,61 @@ def train_model(input_shape):
 	return model, history
 	
 def check(folder, image_name, model):
-	data = np.ndarray(shape = (1, img_rows, img_cols, channels), dtype = np.float32)
+	temp_data = np.ndarray(shape = (1, img_rows, img_cols, channels), dtype = np.float32)
 	img, img_array = None, None
 	if image_name in os.listdir(resized_images + folder):
 		print(resized_images + folder + image_name)
 		img = load_img(resized_images + folder + image_name)
 		img_array = img_to_array(img) # shape is (64, 64, 3)
 		img_array.reshape((img_rows, img_cols, channels))
-		data[0] = img_array
-		print(model.predict(data, verbose = 1))
+		temp_data[0] = img_array
+		print(model.predict(temp_data, verbose = 1))
 	else:
 		print(filename + " not found")
 
-def save_cnn(model):
-	model.save('cnn.h5')
-	del model
+def save_cnn(model, filename):
+	model.save(filename)
+	print("Model saved as " + filename)
 
-def load_cnn():
-	return load_model('cnn.h5')
+def load_cnn(filename):
+	model = load_model(filename)
+	print(filename + " loaded")
+	return model
 
-#resize_images()
-data_train, data_test, labels_train, labels_test, input_shape = create_datasets()
-model, history = train_model(input_shape)
-score = model.evaluate(data_test, labels_test, verbose = 1)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+get_data()
 
-"""plt.plot(history.losses)
-plt.title('Loss during Training')
-plt.ylabel('Loss')
-plt.xlabel('Batch')
-plt.show()"""
+load_name = input("Model to to load?\n") + ".h5"
+if load_name not in ["None.h5", "none.h5"]:
+	model = load_cnn(load_name)
+	test_proportion = float(input("Proportion of data to enter into test set?\n"))
+	if test_proportion != 0:
+		create_datasets(test_proportion = test_proportion)
+		score = model.test_on_batch(data_test, labels_test)
+		print(score)
 
-plt.plot(history.history['acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.show()
-save_cnn(model)
-print("Model saved")
+	else:
+		folder, filename = input("Parasitized or Uninfected?\n") + "\\", input("Filename?\n") + ".png"
 
-"""model = load_cnn()
-print("Loaded model")
+		while folder != "Done":
+			check(folder, filename, model)
+			folder = input("Parasitized or Uninfected?\n")
+			filename = input("Filename?\n")
 
-folder, filename = input("Parasitized or Uninfected?\n") + "\\", input("Filename?\n") + ".png"
+if input("Train new model?\n") in ["Yes", "yes"]:
+	create_datasets(float(input("Proportion of data to enter into test set?\n")))
+	model, history = train_model(input_shape)
+	score = model.evaluate(data_test, labels_test, verbose = 1)
+	print("Test loss: ", score[0])
+	print("Test accuracy: ", score[1])
 
-while folder != "Done":
-	check(folder, filename, model)
-	folder = input("Parasitized or Uninfected?\n")
-	filename = input("Filename?\n")"""
+	plt.plot(history.history["acc"])
+	plt.title("Model accuracy")
+	plt.ylabel("Accuracy")
+	plt.xlabel("Epoch")
+	plt.show()
+	
+	if input("Save the model?\n") in ["Yes", "yes"]:
+		save_cnn(model = model, filename = input("Filename to save the model with?\n") + ".h5")
 
 
 
